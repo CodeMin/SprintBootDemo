f80c61f8ab68b0d7133694a7c39bb3c86d8f8bd9
Min Xu <mxu1@godaddy.com>
2020-09-20 23:40:17 +0800

    Add NotNull annotation to UserDto

--- src/main/java/com/company/springboot/dto/UserDto.java
+++ src/main/java/com/company/springboot/dto/UserDto.java
@@ -2,11 +2,15 @@ package com.company.springboot.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class UserDto {
 
+  @NotNull
   private Long id;
 
+  @NotNull
   private String name;
 
   private String email;
