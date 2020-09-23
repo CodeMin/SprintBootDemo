e3fceb9de3f37b4007060ab5c8ee9cba59f39f84
Min Xu <mxu1@godaddy.com>
2020-09-23 22:36:25 +0800

    Add scheduled task

--- src/main/java/com/company/springboot/SpringBootDemoApplication.java
+++ src/main/java/com/company/springboot/SpringBootDemoApplication.java
@@ -3,9 +3,11 @@ package com.company.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.company.springboot.repository")
+@EnableScheduling
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/company/springboot/tasks/ScheduledTask.java
@@ -0,0 +1,18 @@
+package com.company.springboot.tasks;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class ScheduledTask {
+
+  private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+  @Scheduled(fixedRate = 10000)
+  public void printCurrentTime() {
+    System.out.println("Current time:" + dateFormat.format(new Date()));
+  }
+}
