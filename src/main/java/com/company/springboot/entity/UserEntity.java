852edc7834656ac06ed3f75243343b72999acb55
Min Xu <mxu1@godaddy.com>
2020-10-08 22:14:36 +0800

    Remove unused import

--- src/main/java/com/company/springboot/entity/UserEntity.java
+++ src/main/java/com/company/springboot/entity/UserEntity.java
@@ -6,7 +6,6 @@ import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.io.Serializable;
 import java.util.Date;
 
 /**
